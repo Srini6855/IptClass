214cea6d3ee9b9f24b56529816032d067616a3ad
srin-omr <srinivasanomrbranch@gmail.com>
2025-06-26 07:40:30 +0530

    Updated codes

--- src/test/java/org/sample/Sampleee.java
+++ src/test/java/org/sample/Sampleee.java
@@ -1,8 +1,20 @@
 package org.sample;
 
+import java.util.Scanner;
+
 public class Sampleee {
 	public static void main(String[] args) {
-		System.out.println("Sample");
-	}
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the number of rows: ");
+		int rows = scanner.nextInt();
 
+		for (int i = 1; i <= rows; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+		scanner.close();
+	}
 }
